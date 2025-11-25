17b5c766e0391d0b1df851de8a0c59fbf2a4b61b
Chandraprakash Pandey <cp.o.pandey@gmail.com>
2025-11-25 16:33:40 +0530

    updated frontend URL Deployed

--- backend/controllers/paymentControllers.js
+++ backend/controllers/paymentControllers.js
@@ -45,7 +45,7 @@ export const paymentVerification = async (req,res) => {
 
         await User.findByIdAndUpdate(userId, {subscribed: true, date_of_purchase: date_of_purchase, date_of_expire: date_of_expire}, {new: true});
         
-        return res.redirect(`http://localhost:5173/user`)
+        return res.redirect(`https://karve-mess-frontend.onrender.com/user`)
     }else{
         res.status(404).json({
         success:false
