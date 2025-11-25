a7db916e7dca36db9c282e3232e64d6f486c1d65
Chandraprakash Pandey <cp.o.pandey@gmail.com>
2025-11-25 19:34:35 +0530

    updated

--- backend/index.js
+++ backend/index.js
@@ -20,6 +20,7 @@ mongoose.connect(process.env.MONGO_URL).then(e=> console.log("MongoDB Connected"
 const app = express()
 const PORT = process.env.PORT || 8000;
 
+app.set('trust proxy', 1);
 app.use(express.urlencoded({extended:true}));
 app.use(cors({ origin: `${process.env.FRONTEND_URL}`, credentials: true }));
 app.use(express.json());
